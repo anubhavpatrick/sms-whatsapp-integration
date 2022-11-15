a1f100f686c68fb90cd32792af8f9f5115b21061
Anubhav Patrick <anubhavpatrick@users.noreply.github.com>
2022-11-15 11:33:34 +0530

    Update email_api_twilio.py

--- email_api_twilio.py
+++ email_api_twilio.py
@@ -5,7 +5,7 @@ from credentials import SENDGRID_API_KEY
 
 message = Mail(
     from_email='anubhavpatrick@gmail.com',
-    to_emails='anubhav.patrick@kiet.edu',
+    to_emails='anubhav.patrick@giindia.com',
     subject='Sending with Twilio SendGrid is Fun',
     html_content='<strong>and easy to do anywhere, even with Python</strong>')
 try:
